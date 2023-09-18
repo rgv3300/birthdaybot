f2b22963f61de7e4318afb839264648967d75278
unknown <rgv3300@gmail.com>
2023-09-18 14:54:28 -0400

    adding birthday get function from az ad

---
+++ BirthdayAutomationProj/src/helpers/getUsersBirthday.ts
@@ -0,0 +1,65 @@
+import { InvocationContext } from "@azure/functions";
+
+interface AdUser {
+    birthday: string;
+    email: string;
+}
+
+export async function getAdUsersAndBirthdays(context: InvocationContext): Promise<AdUser[]> {
+    const AD_ACCESS_TOKEN = process.env.AD_ACCESS_TOKEN;
+    let entriesRead = 0;
+    const adUserList = [];
+
+    if (!AD_ACCESS_TOKEN) {
+        throw Error('AD access token not set in the environment.');
+    }
+    // queries received by api    
+
+    try {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${AD_ACCESS_TOKEN}`,
+                'consistencyLevel': 'eventual'
+            }
+        };
+        const url = `https://graph.microsoft.com/v1.0/users?$select=mail,userprincipalname,onPremisesExtensionAttributes&$count=true`
+        context.log(`Making graph api request to ${url}...`)
+        const adResponse = await fetch(url, options);
+        let adJson = await adResponse.json();
+        let hasPaginatedResponse = false;
+
+        if ("@odata.count" in adJson) {
+            entriesRead += adJson["@odata.count"];
+        }
+
+        if ("value" in adJson) {
+            adUserList.push(...adJson.value);
+        } 
+
+        if ("@odata.nextLink" in adJson) {
+            hasPaginatedResponse = true;
+            while (hasPaginatedResponse) {
+                const nextLink = adJson["@odata.nextLink"];
+                context.log(`Making graph api request to ${nextLink}...`)
+                const adNextResponse = await fetch(nextLink, options);
+                const adNextJson = await adNextResponse.json();
+                
+                if ("value" in adNextJson) {
+                    adUserList.push(...adJson.value) ;
+                }
+
+                if (!("@odata.nextLink" in adNextJson)) {
+                    hasPaginatedResponse = false;
+                }
+                adJson = adNextJson;
+            }
+        } 
+        
+    } catch (error) {
+        context.log(error); 
+    }
+    context.log(`${adUserList.length} entries in the ad use list...`)
+    context.log(`${entriesRead} entries read during the function run...`);
+    return adUserList;
+}
