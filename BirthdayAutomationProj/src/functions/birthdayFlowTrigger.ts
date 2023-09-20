728e7dbc4b9a55cb45b240d34de983f9c2c2f8bf
unknown <rgv3300@gmail.com>
2023-09-20 08:46:04 -0400

    adding birthday flow and changing the cron exp to 9 am

--- BirthdayAutomationProj/src/functions/birthdayFlowTrigger.ts
+++ BirthdayAutomationProj/src/functions/birthdayFlowTrigger.ts
@@ -1,10 +1,21 @@
 import { app, InvocationContext, Timer } from "@azure/functions";
+import {getAdUsersAndBirthdays} from '../helpers/getUsersBirthday'
+import {postUsersBirthday} from '../helpers/postUsersBirthday'
+import { isBirthdayToday } from "../helpers/isBirthdayToday";
 
 export async function birthdayFlowTrigger(myTimer: Timer, context: InvocationContext): Promise<void> {
-    context.log('Timer trigger was triggered.')
+    context.log('Timer trigger was triggered.');
+    const adUsers = await getAdUsersAndBirthdays(context);
+    const usersWithBirthday = adUsers.filter(adUser => isBirthdayToday(adUser.birthday));
+    if (usersWithBirthday.length > 0) {
+        for (const user of usersWithBirthday) context.log(`User with email - ${user.email} has birthday today - ${user.birthday}`);
+        const postBirthday = await postUsersBirthday(context, usersWithBirthday);
+    } else {
+        context.log('No birthdays found for today...')
+    }
 }
 
 app.timer('birthdayFlowTrigger', {
-    schedule: '0 */1 * * * *',
+    schedule: '0 0 9 * * *',
     handler: birthdayFlowTrigger
 });
